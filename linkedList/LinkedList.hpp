ebd3975b6f23f3908acc963b7db18e87aaf31ae4
Victor Berger <victorluiz.m4@gmail.com>
2016-02-05 16:22:18 -0600

    Fix size() method

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,10 +29,7 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
-		Fix this method
-	*/
-	return(0);
+	return(m_size);
 }
 
 template <typename T>
@@ -103,9 +100,7 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	
 
 	return(isRemoved);
 }	
